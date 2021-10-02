f27b19d2eea8db5b6f086b4b5b7408c16058dc35
hervebags <hervechubaka@gmail.com>
2021-10-02 15:30:56 -0400

    inor refactoring

--- game-api/src/main/java/ca/ulaval/glo4002/game/interfaces/rest/food/FoodDTO.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/interfaces/rest/food/FoodDTO.java
@@ -1,8 +1,5 @@
 package ca.ulaval.glo4002.game.interfaces.rest.food;
 
-import com.fasterxml.jackson.annotation.JsonFilter;
-
-@JsonFilter("positiveValuesFilter")
 public class FoodDTO {
 
     public int qtyBurger = 0;
