e94a258a9b6c4e2ffa5322502ef4b168c56cbcc1
hervebags <hervechubaka@gmail.com>
2021-10-02 15:25:18 -0400

    Correction de InvalidRessourceQuantityException

--- game-api/src/main/java/ca/ulaval/glo4002/game/interfaces/rest/food/FoodDTO.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/interfaces/rest/food/FoodDTO.java
@@ -1,5 +1,8 @@
 package ca.ulaval.glo4002.game.interfaces.rest.food;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+
+@JsonFilter("positiveValuesFilter")
 public class FoodDTO {
 
     public int qtyBurger = 0;
