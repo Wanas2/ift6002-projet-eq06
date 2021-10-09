90c116fd0026de1d001e3c324e95ca550a28a392
naoussi-sijou <wilfried-armand.naoussi-sijou.1@ulaval.ca>
2021-10-10 00:34:59 +0100

    Changement du test de AddDinosaurAction

--- game-api/src/test/java/ca/ulaval/glo4002/game/domain/action/AddDinosaurActionTest.java
+++ game-api/src/test/java/ca/ulaval/glo4002/game/domain/action/AddDinosaurActionTest.java
@@ -1,40 +1,27 @@
 package ca.ulaval.glo4002.game.domain.action;
 
 import ca.ulaval.glo4002.game.domain.dinosaur.*;
-import ca.ulaval.glo4002.game.domain.dinosaur.consumption.FoodConsumptionStrategy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class AddDinosaurActionTest {
-    private final Species A_SPECIES = Species.Allosaurus;
-    private final int WEIGHT = 5;
-    private final String A_NAME= "DinoA";
-    private final Gender A_GENDER = Gender.F;
-    private FoodConsumptionStrategy foodConsumptionStrategy;
-    private DinosaurRepository dinosaurRepository;
     private Dinosaur dinosaur;
     private Herd herd;
     private AddDinosaurAction addDinosaurAction;
 
     @BeforeEach
     public void setUp() {
-        foodConsumptionStrategy = mock(FoodConsumptionStrategy.class);
-        dinosaurRepository = mock(DinosaurRepository.class);
-        dinosaur = new Dinosaur(A_SPECIES, WEIGHT, A_NAME, A_GENDER, foodConsumptionStrategy);
-        herd = new Herd(dinosaurRepository);
+        dinosaur = mock(Dinosaur.class);
+        herd = mock(Herd.class);
         addDinosaurAction = new AddDinosaurAction(herd, dinosaur);
     }
 
     @Test
     public void givenADinosaur_WhenExecute_thenDinosaurIsAddedToHerd() {
-        when(dinosaurRepository.findAll()).thenReturn(new ArrayList<>());
         addDinosaurAction.execute();
-        assertFalse(herd.isEmpty());
+
+        verify(herd).add(dinosaur);
     }
 }
