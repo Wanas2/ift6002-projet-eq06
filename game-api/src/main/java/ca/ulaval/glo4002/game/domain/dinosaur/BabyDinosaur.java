19f3dd042f6d6f3175b6019d802e8b224f3ebd47
Toba Richard <richardotoba@gmail.com>
2021-10-12 23:08:53 -0400

    Ameliorations pour PR #89
    Une legère amélioration pour le cas de BabyDinosaurFactory (mais toujours non satisfaisant)

--- game-api/src/main/java/ca/ulaval/glo4002/game/domain/dinosaur/BabyDinosaur.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/domain/dinosaur/BabyDinosaur.java
@@ -6,11 +6,12 @@ public class BabyDinosaur extends Dinosaur{
 
     protected Dinosaur fatherDinosaur;
     protected Dinosaur motherDinosaur;
+    protected final static int INITIAL_BABY_WEIGHT = 1;
 
     public BabyDinosaur(Species species, String name, Gender gender,
                         FoodConsumptionStrategy foodConsumptionStrategy, Dinosaur fatherDinosaur,
                         Dinosaur motherDinosaur) {
-        super(species, 1, name, gender, foodConsumptionStrategy);
+        super(species, INITIAL_BABY_WEIGHT, name, gender, foodConsumptionStrategy);
         this.fatherDinosaur = fatherDinosaur;
         this.motherDinosaur = motherDinosaur;
     }
