3b915e90b2915b660e40dc05376449ce744d1509
hervebags <hervechubaka@gmail.com>
2021-10-13 22:26:56 -0400

    Ajout de quelques tests

--- game-api/src/test/java/ca/ulaval/glo4002/game/domain/GameTest.java
+++ game-api/src/test/java/ca/ulaval/glo4002/game/domain/GameTest.java
@@ -3,7 +3,9 @@ package ca.ulaval.glo4002.game.domain;
 import static org.junit.jupiter.api.Assertions.*;
 
 
+import ca.ulaval.glo4002.game.domain.action.AddDinosaurAction;
 import ca.ulaval.glo4002.game.domain.action.AddFoodAction;
+import ca.ulaval.glo4002.game.domain.dinosaur.Dinosaur;
 import ca.ulaval.glo4002.game.domain.dinosaur.Herd;
 import ca.ulaval.glo4002.game.domain.food.FoodType;
 import ca.ulaval.glo4002.game.domain.food.Pantry;
@@ -20,6 +22,7 @@ class GameTest {
 
     private Turn turn;
     private Herd herd;
+    private Dinosaur aDinosaur;
     private Game game;
     private Pantry pantry;
     private Food aFoodItem1;
@@ -32,6 +35,7 @@ class GameTest {
         initializesFood();
         turn = mock(Turn.class);
         herd = mock(Herd.class);
+        aDinosaur = mock(Dinosaur.class);
         pantry = mock(Pantry.class);
         game = new Game(herd, pantry, turn);
     }
@@ -41,6 +45,13 @@ class GameTest {
         game.addFood(food);
 
         verify(turn).acquireNewAction(any(AddFoodAction.class));
+    }
+
+    @Test
+    public void whenAddDino_thenTurnShouldAcquireANewAction() {
+        game.addDinosaur(aDinosaur);
+
+        verify(turn).acquireNewAction(any(AddDinosaurAction.class));
     }
 
     @Test
