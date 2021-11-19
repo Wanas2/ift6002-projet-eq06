b379d36cd78ec568e3ef708b82787f6ccddeca3b
lucasThibord <leptitlucki@hotmail.fr>
2021-11-19 15:57:48 -0500

    Deleting tests that are not supposed to be there

--- game-api/src/test/java/ca/ulaval/glo4002/game/applicationService/DinosaurServiceTest.java
+++ game-api/src/test/java/ca/ulaval/glo4002/game/applicationService/DinosaurServiceTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
