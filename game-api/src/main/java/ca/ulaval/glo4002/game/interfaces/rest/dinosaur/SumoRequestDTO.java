33f715eb7a4f47941b9335724a3aa1b08940c666
unknown <yohak3@ulaval.ca>
2021-11-20 12:38:10 -0500

    added final to SumoRequestDTO

--- game-api/src/main/java/ca/ulaval/glo4002/game/interfaces/rest/dinosaur/SumoRequestDTO.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/interfaces/rest/dinosaur/SumoRequestDTO.java
@@ -2,6 +2,11 @@ package ca.ulaval.glo4002.game.interfaces.rest.dinosaur;
 
 public class SumoRequestDTO {
 
-    public String challenger;
-    public String challengee;
+    public final String challenger;
+    public final String challengee;
+
+    public SumoRequestDTO(String challenger, String challengee) {
+        this.challenger = challenger;
+        this.challengee = challengee;
+    }
 }
