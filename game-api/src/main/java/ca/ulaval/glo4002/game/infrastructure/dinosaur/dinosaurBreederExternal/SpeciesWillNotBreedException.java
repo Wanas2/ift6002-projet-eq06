4d0b78164e330b19f5ba327975e41dea77f27110
Toba Richard <richardotoba@gmail.com>
2021-11-24 17:02:06 -0500

    Conventions

--- game-api/src/main/java/ca/ulaval/glo4002/game/infrastructure/dinosaur/dinosaurBreederExternal/SpeciesWillNotBreedException.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/infrastructure/dinosaur/dinosaurBreederExternal/SpeciesWillNotBreedException.java
@@ -3,6 +3,7 @@ package ca.ulaval.glo4002.game.infrastructure.dinosaur.dinosaurBreederExternal;
 public class SpeciesWillNotBreedException extends Exception {
 
     private static final String MESSAGE = "Impossible to breed these species";
+
     public SpeciesWillNotBreedException() {
         super(MESSAGE);
     }
