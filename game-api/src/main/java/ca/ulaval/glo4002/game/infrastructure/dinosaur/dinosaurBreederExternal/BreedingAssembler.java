17b7ba76e03fda5593e990a0ac0f3ec52a3c6355
unknown <yohak3@ulaval.ca>
2021-11-24 09:34:40 -0500

    formatting

--- game-api/src/main/java/ca/ulaval/glo4002/game/infrastructure/dinosaur/dinosaurBreederExternal/BreedingAssembler.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/infrastructure/dinosaur/dinosaurBreederExternal/BreedingAssembler.java
@@ -5,6 +5,7 @@ import ca.ulaval.glo4002.game.domain.dinosaur.Dinosaur;
 public class BreedingAssembler {
 
     public BreedingRequestExternalDTO toDTO(Dinosaur fatherDinosaur, Dinosaur motherDinosaur) {
-        return new BreedingRequestExternalDTO(fatherDinosaur.getSpecies().toString(), motherDinosaur.getSpecies().toString());
+        return new BreedingRequestExternalDTO(fatherDinosaur.getSpecies().toString(),
+                motherDinosaur.getSpecies().toString());
     }
 }
