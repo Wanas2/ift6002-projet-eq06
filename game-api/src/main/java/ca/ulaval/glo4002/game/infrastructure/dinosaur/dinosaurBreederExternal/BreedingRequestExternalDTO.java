ee3bd1f7a554348a0d7f8f3dfafd18bc145ccd30
unknown <yohak3@ulaval.ca>
2021-11-24 10:46:58 -0500

    fixed JsonCreator

--- game-api/src/main/java/ca/ulaval/glo4002/game/infrastructure/dinosaur/dinosaurBreederExternal/BreedingRequestExternalDTO.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/infrastructure/dinosaur/dinosaurBreederExternal/BreedingRequestExternalDTO.java
@@ -1,10 +1,13 @@
 package ca.ulaval.glo4002.game.infrastructure.dinosaur.dinosaurBreederExternal;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class BreedingRequestExternalDTO {
 
     public final String fatherSpecies;
     public final String motherSpecies;
 
+    @JsonCreator
     public BreedingRequestExternalDTO(String fatherSpecies, String motherSpecies) {
         this.fatherSpecies = fatherSpecies;
         this.motherSpecies = motherSpecies;
--- game-api/src/main/java/ca/ulaval/glo4002/game/interfaces/rest/dinosaur/BreedingRequestDTO.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/interfaces/rest/dinosaur/BreedingRequestDTO.java
@@ -1,12 +1,18 @@
 package ca.ulaval.glo4002.game.interfaces.rest.dinosaur;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class BreedingRequestDTO {
 
     public final String name;
     public final String fatherName;
     public final String motherName;
 
-    public BreedingRequestDTO(String name, String fatherName, String motherName) {
+    @JsonCreator
+    public BreedingRequestDTO(@JsonProperty(value = "name", required = true) String name,
+                              @JsonProperty(value = "fatherName", required = true) String fatherName,
+                              @JsonProperty(value = "motherName", required = true) String motherName) {
         this.name = name;
         this.fatherName = fatherName;
         this.motherName = motherName;
