78ff7512ce6b540d35b791c4841e3eef280a6cf7
hervebags <hervechubaka@gmail.com>
2021-11-24 22:18:31 -0500

    Modification du test dans AdultDinosaurTest

--- game-api/src/test/java/ca/ulaval/glo4002/game/domain/dinosaur/AdultDinosaurTest.java
+++ game-api/src/test/java/ca/ulaval/glo4002/game/domain/dinosaur/AdultDinosaurTest.java
@@ -1,7 +1,6 @@
 package ca.ulaval.glo4002.game.domain.dinosaur;
 
 import ca.ulaval.glo4002.game.domain.dinosaur.consumption.FoodConsumptionStrategy;
-import ca.ulaval.glo4002.game.domain.dinosaur.consumption.FoodNeed;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +9,7 @@ import static org.mockito.Mockito.mock;
 
 public class AdultDinosaurTest {
 
-    private final static int WEIGHT_VARIATION = 27;
+    private final static int A_POSITIVE_WEIGHT_VARIATION = 27;
 
     private AdultDinosaur aDinosaur;
 
@@ -19,17 +18,21 @@ public class AdultDinosaurTest {
         FoodConsumptionStrategy aFoodConsumptionStrategy = mock(FoodConsumptionStrategy.class);
         int dinosaurWeight = 150;
         String dinosaurName = "Bobi";
-        aDinosaur = new AdultDinosaur(Species.Ankylosaurus, dinosaurWeight, "dinosaurName", Gender.F,
+        aDinosaur = new AdultDinosaur(Species.Ankylosaurus, dinosaurWeight, dinosaurName, Gender.F,
                 aFoodConsumptionStrategy);
     }
 
     @Test
-    public void givenWeightVariation_whenModifyWeight_thenWeightShouldBeModified() {
-        int expectedWeight = 177;
+    void givenAPositiveWeightVariation_whenModifyWeight_thenTheWeightVariationShouldBeAddedToTheDinosaurWeight(){
+        int dinosaurWeightAfterWeightIncrease = aDinosaur.getWeight() + A_POSITIVE_WEIGHT_VARIATION;
 
-        aDinosaur.modifyWeight(WEIGHT_VARIATION);
-        int modifiedWeight = aDinosaur.getWeight();
+        aDinosaur.modifyWeight(A_POSITIVE_WEIGHT_VARIATION);
+        int newDinosaurWeight = aDinosaur.getWeight();
 
-        assertEquals(expectedWeight, modifiedWeight);
+        assertEquals(dinosaurWeightAfterWeightIncrease, newDinosaurWeight);
+    }
+
+    @Test void givenANegativeWeightVariation_whenModifyWeight_thenTheWeightVariationShouldBeSubtractedToTheDinosaurWeight(){
+
     }
 }
